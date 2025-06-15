85647b242262853eac0c4e802630319dc554fcda
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-15 14:58:14 -0400

    se uso NoteCards en NotePage

--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -1,6 +1,28 @@
+import { useContext } from "react"
+import NoteCards from "../components/NoteCards";
+import { NoteContext } from "../context/notecontext";
+
 function NotePage() {
+  const {notes} = useContext(NoteContext);
+
+  const valueNoteCards = notes.map(valueNotes => {
+    return(
+      <li key={valueNotes.id}>
+        <NoteCards note={valueNotes}/>
+      </li>
+    )
+  })
+
   return (
-    <div>NotePage</div>
+    <div style={{
+      textAlign: 'center'
+    }}>
+      <h2>Notes</h2>
+
+      <ul className="note-list">
+        {valueNoteCards}
+      </ul>
+    </div>
   )
 }
 

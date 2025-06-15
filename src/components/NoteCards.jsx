35fa2f5191b43ce99a0d197efccda02fa09e40e0
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-15 16:41:55 -0400

    Uso del NoteContext en el NoteCards para los inputs

--- src/components/NoteCards.jsx
+++ src/components/NoteCards.jsx
@@ -1,17 +1,41 @@
+import { useContext } from 'react'
 import './NoteCard.css'
+import { NoteContext } from '../context/notecontext'
 
 function NoteCards(props) {
-  return (
+  const {note} = props
 
+  const {updateNote} = useContext(NoteContext);
+
+  const handleInput = (event) => {
+    const updateValueNote = {
+      ...note, 
+      title: event.target.value
+    };
+    updateNote(updateValueNote);
+  }
+
+  const handleCheck = (event) => {
+    const updateValueNote = {
+      ...note, 
+      marked: !note.marked
+    };
+
+    updateNote(updateValueNote);
+  }
+
+  return (
     <article className="note-card">
       <input 
         type="text" 
         className="card-title" 
-        value={props.note.title} 
+        value={note.title} 
+        onChange={handleInput}
       />
       <input 
         type="checkbox" 
-        checked={props.note.marked}
+        checked={note.marked}
+        onChange={handleCheck}
       />
     </article>
     
